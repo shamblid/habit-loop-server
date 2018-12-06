a992b2d509c33422f399272bcb6ef9d430766328
DJ <hamblindj@gmail.com>
2018-12-05 23:59:44 -0800

    add serverless stuff

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express'); 
+const serverless = require('serverless-http');
 const { ApolloServer, gql } = require('apollo-server-express');
 const { typeDefs, resolvers } = require('./api');
 const PORT = process.env.PORT || 4000;
@@ -7,6 +8,13 @@ const app = express();
 const server = new ApolloServer({ typeDefs, resolvers });
 server.applyMiddleware({ app });
 
+
+
+
 app.listen(PORT, () =>
 	console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
 );
+
+const handler = serverless(app)
+
+module.exports.handler = handler;
