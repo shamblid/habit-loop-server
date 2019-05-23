92166d1e25bf163f26df871b35211a33803ff3ce
donaldjr <hamblindj@gmail.com>
2019-05-22 21:41:46 -0700

    see if redis works

--- app.js
+++ app.js
@@ -4,9 +4,18 @@ const api = require('./api');
 
 const server = new ApolloServer(api);
 
-exports.graphql = server.createHandler({
-  cors: {
-    origin: '*',
-    credentials: true,
-  },
+const runApollo = (event, context, apollo) => new Promise((resolve, reject) => {
+  const callback = (error, body) => (error ? reject(error) : resolve(body));
+  apollo(event, context, callback);
 });
+
+exports.graphql = async (event, context) => {
+  const apollo = server.createHandler({
+    cors: {
+      origin: '*',
+      credentials: true,
+    },
+  });
+  const response = await runApollo(event, context, apollo);
+  return response;
+};
